25432d517a0c41c082549c3bd6722fdf00c54920
didar <deddarnawzad@gmail.com>
2024-01-14 21:03:58 +0300

    Add new SVG icons and update LeftSidebar layout

--- src/layouts/LeftSidebar.vue
+++ src/layouts/LeftSidebar.vue
@@ -12,7 +12,7 @@
       <slot></slot>
     </div>
 
-    <div class="drawer-side">
+    <div class="drawer-side p-0">
       <label
         for="my-drawer-4"
         aria-label="close sidebar"
@@ -20,7 +20,7 @@
       ></label>
       <ul
         :class="
-          'min-h-full p-4 menu bg-base-200 text-base-content ' +
+          'min-h-full  menu bg-base-200 text-base-content p-4 ' +
           (isExpanded ? 'w-20' : 'w-80')
         "
       >
@@ -50,9 +50,33 @@
           :to="`${route.path}`"
           class="p-3 hover:bg-base-300 rounded-md text-[15px]"
         >
-          <li>{{ route.name }}</li>
+          <li>{{ isExpanded ? "icon" : route.name }}</li>
         </router-link>
-        <button class="btn  bg-white w-10" @click="toggle">x</button>
+
+        <div>
+          <label for="expand" class="btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 "
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+              />
+            </svg>
+          </label>
+          <input
+            id="expand"
+            type="checkbox"
+            @click="toggle"
+            class="hidden"
+          />
+        </div>
       </ul>
     </div>
   </div>
@@ -68,19 +92,19 @@ const toggle = () => {
 
 const routes = ref([
   {
-    name: "Profile",
+    name: "Profile ",
     path: "/profile",
   },
   {
-    name: "My feed",
+    name: "feed",
     path: "/my-feed",
   },
   {
-    name: "HOWTO",
+    name: "HOWTO ",
     path: "/how-to",
   },
   {
-    name: "bookmarks",
+    name: "bookmarks ",
     path: "/bookmarks",
   },
 ]);
