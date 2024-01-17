5c8d54303b86ea5f6297aea816c5ef3c34f65b46
didar <deddarnawzad@gmail.com>
2024-01-17 22:45:19 +0300

    Update BlogPost.vue with content and image

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,7 +1,40 @@
 <template lang="">
-  <div>blog post</div>
+  <!-- <div>{{$route.params.id}}</div> -->
+  <div class="content">
+    <div class="prose ">
+        <h1>Blog Post Title</h1>
+        <figure>
+            <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt=""
+            />
+            <figcaption>Photo by John Doe</figcaption>
+        </figure>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatum, quibusdam, quia, voluptate voluptas quod dolorum
+          voluptatibus quos quas quidem voluptatem? Quisquam voluptatum,
+          quibusdam, quia, voluptate voluptas quod dolorum voluptatibus quos
+          quas quidem voluptatem?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatum, quibusdam, quia, voluptate voluptas quod dolorum
+          voluptatibus quos quas quidem voluptatem? Quisquam voluptatum,
+          quibusdam, quia, voluptate voluptas quod dolorum voluptatibus quos
+          quas quidem voluptatem?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatum, quibusdam, quia, voluptate voluptas quod dolorum
+          voluptatibus quos quas quidem voluptatem? Quisquam voluptatum,
+          quibusdam, quia, voluptate voluptas quod dolorum voluptatibus quos
+          quas quidem voluptatem?
+        </p>
+        
+          </div>
+    </div>
 </template>
-<script>
-export default {};
+<script setup>
 </script>
 <style lang=""></style>
