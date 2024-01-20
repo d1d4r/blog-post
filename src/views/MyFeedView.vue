64b4d51db69155c1180fd7ca603cf08c5ecc35fd
didar <deddarnawzad@gmail.com>
2024-01-20 16:25:33 +0300

    Add loading skeleton for MyFeedView

--- src/views/MyFeedView.vue
+++ src/views/MyFeedView.vue
@@ -1,10 +1,14 @@
 <template lang="">
-  <div class="flex flex-wrap justify-evenly gap-7">
-    <CardPost v-for="item in state.posts" :key="item.id" :item="item" />
+  <div class="flex flex-wrap justify-evenly gap-7" v-if="!state.loading">
+    <CardPost  v-for="item in state.posts" :key="item.id" :item="item" />
+  </div>
+  <div class="flex flex-wrap justify-evenly gap-7" v-if="state.loading">
+    <SkeletonCardPost  v-for="item in 200" :key="item.id"  />
   </div>
 </template>
 <script setup>
 import CardPost from "@/components/CardPost.vue";
+import SkeletonCardPost from "@/components/SkeletonCardPost.vue";
 import Post from "@/service/firestore/post.js";
 import { app } from "@/firebase/index.js";
 import { reactive, onMounted } from "vue";
