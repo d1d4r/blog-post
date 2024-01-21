e6e690c36ac09042318c2352923363da63ddc80b
didar <deddarnawzad@gmail.com>
2024-01-21 23:35:03 +0300

    Remove unused code from firebase index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -15,4 +11,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-//export const db = app.firestore(); 
