1fd214a2d0ca0fc593a628fa2e005c3b3f0962de
didar <deddarnawzad@gmail.com>
2024-01-22 19:53:19 +0300

    Delete unused layout components

--- src/layouts/MainContent.vue
+++
@@ -1,8 +0,0 @@
-<template lang="">
-  <main>
-    <router-view></router-view>
-    <!-- <router-view name="helper"></router-view> -->
-  </main>
-</template>
-<script setup></script>
-<style lang=""></style>
