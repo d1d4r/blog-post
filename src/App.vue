f4321ccba118b37f69d2bd419433e83825da8ea7
didar <deddarnawzad@gmail.com>
2024-01-22 19:49:03 +0300

    Refactor App.vue to use router-view instead of LayoutWrapper

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <!-- <RootView /> -->
-  <LayoutWrapper />
+  <!-- <LayoutWrapper /> -->
+
+  <router-view></router-view>
 </template>
 <script setup>
 import LayoutWrapper from "@/layouts/LayoutWrapper.vue";
-import RootView from "./views/RootView.vue";
+import RootView from "@/views/RootView.vue";
 </script>
 
 <style scoped></style>
