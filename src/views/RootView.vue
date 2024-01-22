fe3abcdc633060b9e3b6c83d76d0b005674cb64c
didar <deddarnawzad@gmail.com>
2024-01-22 19:48:45 +0300

    Update RootView.vue with hero section and prose text

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -1,11 +1,36 @@
 <template lang="">
-  <div>
-    root
-    <router-link to="/how-to" class="btn  w-2/5 ">HOWTO</router-link>
-    <router-link to="/" class="btn  w-2/5 ">back</router-link>
+ 
+  <div
+    class="hero min-h-screen"
+    style="
+      background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);
+    "
+  >
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+        <p class="mb-5">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+        <router-link to="/how-to" class="btn w-2/5">HOWTO</router-link>
+      </div>
+    </div>
   </div>
+
+  <div class="flex flex-col items-center justify-center py-20">
+    <div class="prose text-center">
+      <h1>Hello there</h1>
+      <p>
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+    </div>
+  </div>
+  
 </template>
-<script setup>
-import LayoutWrapper from "@/layouts/LayoutWrapper.vue";
-</script>
+<script setup></script>
 <style lang=""></style>
