45fab5f5f586aa7536148171dd33b43c8177bacc
didar <deddarnawzad@gmail.com>
2024-01-22 19:49:55 +0300

    Add Layout.vue component

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template lang="">
+  <TopNavigation />
+  <div class="drawer md:drawer-open">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <MainConten />
+    <LeftSide />
+  </div>
+</template>
+<script setup>
+import LeftSide from "./LeftSide.vue";
+import MainConten from "./MainConten.vue";
+import TopNavigation from "./TopNavigation.vue";
+</script>
+<style lang=""></style>
