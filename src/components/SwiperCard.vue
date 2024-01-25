95e492707e877a9bed0ca9f0e7933cf8cce76947
didar <deddarnawzad@gmail.com>
2024-01-26 00:23:38 +0300

    Update SwiperCard component configuration

--- src/components/SwiperCard.vue
+++ src/components/SwiperCard.vue
@@ -1,17 +1,19 @@
 <template lang="">
-  <Swiper
-    :slides-per-view="3"
-    :space-between="spaceBetween"
-    :centered-slides="true"
-    class="bg-white w-[90%] h-[90%] p-10 m-10"
-    :virtual="true"
-    :navigation="true"
-    :loop="true"
-    @swiperprogress="onProgress"
-    @swiperslidechange="onSlideChange"
-  >
-    <slot></slot>
-  </Swiper>
+  
+    <Swiper
+      :slides-per-view="5"
+      :space-between="spaceBetween"
+      :centered-slides="true"
+      class="border-white border-[0.01px] w-[90%] m-10"
+      :navigation="true"
+      @swiperprogress="onProgress"
+      @swiperslidechange="onSlideChange"
+    >
+       <router-link to="/main/my-feed" >see more</router-link>
+      <slot></slot>
+     
+    </Swiper>
+  
 </template>
 <script setup>
 import "swiper/css";
@@ -22,7 +24,7 @@ import { register } from "swiper/element/bundle";
 import { Swiper, SwiperSlide } from "swiper/vue";
 
 register();
-const spaceBetween = 10;
+const spaceBetween = 50;
 const onProgress = (e) => {
   const [swiper, progress] = e.detail;
   console.log(progress);
