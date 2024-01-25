3be45a56959305d3d1abc8f288f288b0ecd96dde
didar <deddarnawzad@gmail.com>
2024-01-25 22:30:25 +0300

    Refactor RootView.vue and add SwiperCard component

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -1,12 +1,12 @@
 <template lang="">
   <div
-    class="hero min-h-screen"
+    class="min-h-screen hero"
     style="
       background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);
     "
   >
     <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content">
+    <div class="text-center hero-content text-neutral-content">
       <div class="max-w-md">
         <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
         <p class="mb-5">
@@ -14,14 +14,60 @@
           excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
           id nisi.
         </p>
-        <router-link to="/main/my-feed" class="btn w-2/5"
+        <router-link to="/main/my-feed" class="w-2/5 btn"
           >strat reading & writing</router-link
         >
       </div>
     </div>
   </div>
 
-  <div class="flex flex-col items-center justify-center py-20">
+  <SwiperCard>
+    <swiper-slide :key="item.id" v-for="item in state.posts">
+      <CardPost :item="item" />
+    </swiper-slide>
+  </SwiperCard>
+
+  <SwiperCard>
+    <swiper-slide :key="item.id" v-for="item in state.posts">
+      <CardPost :item="item" />
+    </swiper-slide>
+  </SwiperCard>
+</template>
+<script setup>
+import SwiperCard from "@/components/SwiperCard.vue";
+import CardPost from "@/components/CardPost.vue";
+import { SwiperSlide } from "swiper/vue";
+
+import Post from "@/service/firestore/post.js";
+import { reactive, onMounted } from "vue";
+
+//register();
+
+const post = new Post();
+
+const state = reactive({
+  posts: [],
+  headers: [],
+  loading: false,
+  error: null,
+});
+
+onMounted(async () => {
+  try {
+    state.loading = true;
+    state.posts = await post.getAll();
+    //console.log("🚀 ~ onMounted ~  state.posts:", state.posts);
+    state.loading = false;
+  } catch (error) {
+    console.log("🚀 ~ onMounted ~ error:", error);
+    state.error = error;
+  } finally {
+    state.loading = false;
+  }
+});
+</script>
+<style lang=""></style>
+<!-- <div class="flex flex-col items-center justify-center py-20">
     <div class="prose text-center">
       <h1>Hello there</h1>
       <p>
@@ -30,7 +76,4 @@
         id nisi.
       </p>
     </div>
-  </div>
-</template>
-<script setup></script>
-<style lang=""></style>
+  </div> -->
