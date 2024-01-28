504bc3a1e1092d72269df318d301ed9d223712d7
didar <deddarnawzad@gmail.com>
2024-01-28 20:48:10 +0300

    Add modal popup and footer

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
   <!-- <LayoutWrapper /> -->
 
   <router-view></router-view>
+  <footer class="p-10 text-center text-white border-t-2 border-base-200 bg-base-500">
+    <p>© 2021 - All rights reserved</p>
+  </footer>
 </template>
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped></style>
--- src/components/ModalPopup.vue
+++ src/components/ModalPopup.vue
@@ -1,30 +1,38 @@
 <template lang="">
   <!-- You can open the modal using ID.showModal() method -->
   <!-- You can open the modal using ID.showModal() method -->
-  <button class="btn" @click="showModal()">open modal</button>
-  <dialog id="my_modal_3" class="modal">
-    <div class="modal-box">
-      <form method="dialog">
-        <button
-          class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 border border-white"
-        >
-          Esc
-        </button>
-      </form>
-      <div class="prose">
-        <h3 class="font-bold text-lg">log account to show bookmarks</h3>
-        <router-link to="/register" class="btn"><span>log account</span></router-link>
+
+  <Teleport to="#modalPortal">
+    <dialog id="my_modal_3" class="modal">
+      <div class="modal-box">
+        <form method="dialog">
+          <button
+            class="absolute border border-white btn btn-sm btn-circle btn-ghost right-2 top-2"
+          >
+            Esc
+          </button>
+        </form>
+        <div class="prose">
+          <h3 class="text-lg font-bold">log account to show bookmarks</h3>
+          <router-link to="/register" class="btn"
+            ><span>log account</span></router-link
+          >
+        </div>
       </div>
-    </div>
-    <form method="dialog" class="modal-backdrop">
-      <button>close</button>
-    </form>
-  </dialog>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  </Teleport>
 </template>
 <script setup>
+import { onMounted } from "vue";
 const showModal = () => {
   const modal = document.getElementById("my_modal_3");
   modal.setAttribute("open", true);
 };
+onMounted(() => {
+  showModal();
+});
 </script>
 <style lang=""></style>
