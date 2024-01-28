7a04c14ca728a502e0a855053ed84c29ff8608ed
didar <deddarnawzad@gmail.com>
2024-01-28 20:24:00 +0300

    Add LoadingOverlay component

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,8 @@
+<template lang="">
+  <div class="absolute inset-0 rounded-md backdrop-blur-lg "></div>
+  <div class="absolute inset-0 flex items-center justify-center">
+    <h2 class="text-3xl font-bold text-white">Loading...</h2>
+  </div>
+</template>
+<script setup></script>
+<style lang=""></style>
