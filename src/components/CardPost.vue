f5db7949043b4123bf3c843550faa2d121d63435
didar <deddarnawzad@gmail.com>
2024-01-28 20:24:14 +0300

    Refactor CardPost component styling

--- src/components/CardPost.vue
+++ src/components/CardPost.vue
@@ -3,11 +3,11 @@
     class="card w-72 bg-base-100 border-solid m-auto lg:m-0 border-[1px] border-sky-500 shadow-xl"
   >
     <div class="card-body">
-      <figure>
+      <figure >
         <img
           :src="props.item.imageUrl"
           alt="Shoes"
-          class="w-10 h-10 rounded-full border-2 border-white shadow-xl"
+          class="w-8 h-8 border-2 border-white rounded-full shadow-xl"
         />
       </figure>
 
@@ -15,18 +15,18 @@
         :to="`/my-feed/blogpost/${props.item.id}`"
         class="hover:underline"
       >
-        <h2 class="card-title p-2">{{ props.item.title }}</h2>
+        <h2 class="p-2 card-title">{{ props.item.title }}</h2>
       </router-link>
       <h2 class="pl-2 text-sm">{{ props.item.date }}</h2>
-      <figure>
+      <figure >
         <img
           loading="lazy"
           :src="props.item.imageUrl"
           alt="Shoes"
-          class="w-64 h-56 rounded-xl object-cover"
+          class="object-cover m-auto select-none h-44 w-44 rounded-xl"
         />
       </figure>
-      <div class="card-actions justify-end p-3">
+      <div class="justify-end p-3 card-actions">
         <div
           class="badge badge-outline"
           v-for="tag in props.item.tags"
