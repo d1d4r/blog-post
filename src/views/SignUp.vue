7b77b680c03d4fb55ad5f6f405aa420b01ae4248
didar <deddarnawzad@gmail.com>
2024-02-04 22:12:33 +0300

    Refactor SignUp.vue component layout and add display name field

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,11 +1,11 @@
 <template lang="">
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content flex-col lg:flex-row-reverse items-stretch">
-      <div class="text-center lg:text-left prose">
+  <div class="min-h-screen hero bg-base-200">
+    <div class="flex-col items-stretch hero-content lg:flex-row-reverse">
+      <div class="prose text-center lg:text-left">
         <h1>regesiter now!</h1>
         <p>create your account now and start writing your own blog</p>
       </div>
-      <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="w-full max-w-sm shadow-2xl card shrink-0 bg-base-100">
         <form @submit.prevent="submit" class="card-body">
           <!-- <div class="form-control">
             <label class="label">
@@ -18,6 +18,18 @@
               required
             />
           </div> -->
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">full name</span>
+            </label>
+            <input
+              v-model="userData.displayname"
+              type="text"
+              placeholder="your name"
+              class="input input-bordered"
+              required
+            />
+          </div>
           <div class="form-control">
             <label class="label">
               <span class="label-text">Email</span>
@@ -62,7 +74,7 @@
               >
             </label>
           </div> -->
-          <div class="form-control mt-6">
+          <div class="mt-6 form-control">
             <button type="submit" class="btn btn-primary">Signup</button>
           </div>
         </form>
@@ -74,20 +86,24 @@
 //TODO: add fix signup
 import { reactive, ref } from "vue";
 import { useAuthenticationStore } from "@/stores/useAuthenticationStore.js";
+import { useRouter } from "vue-router";
 
 const { signup } = useAuthenticationStore();
 const errorMessage = ref(null);
 
+const router = useRouter();
+
 const userData = reactive({
+  displayname: "",
   email: "",
   password: "",
 });
 
-
-const submit =  () => {
+const submit = async () => {
   errorMessage.value = null;
   try {
-     signup(userData.email, userData.password);
+    await signup(userData.email, userData.password, userData.displayname);
+    router.push("/main/my-feed");
   } catch (error) {
     switch (error.code) {
       case "auth/email-already-in-use":
