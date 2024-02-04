d0a70758e7ae22de2c6708eba6a348e3f9cbfd40
didar <deddarnawzad@gmail.com>
2024-02-04 22:12:56 +0300

    Refactor ProfileImage component UI

--- src/components/ProfileImage.vue
+++ src/components/ProfileImage.vue
@@ -4,16 +4,13 @@
       <input
         type="text"
         placeholder="Search"
-        class="input input-bordered w-24 md:w-auto"
+        class="w-24 input input-bordered md:w-auto"
       />
     </div>
     <div class="dropdown dropdown-end" v-if="isLogged">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-          />
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <div class="rounded-full">
+          <!-- {{ currentUser }} -->
         </div>
       </div>
       <ul
@@ -21,31 +18,26 @@
         class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
       >
         <li>
-          <a class="justify-between">
-            Profile
-            <span class="badge">New</span>
-          </a>
+          <span class="btn" @click="getCurrentUser">show user</span>
         </li>
         <li><a>Settings</a></li>
         <li @click="handleSignOut" class="btn btn-ghost">sign out</li>
       </ul>
     </div>
     <div class="dropdown dropdown-end" v-else>
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img
-            alt=" "
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-          />
-        </div>
+      <div
+        tabindex="0"
+        role="button"
+        class="btn btn-ghost btn-circle bg-base-300"
+      >
+        <div class="rounded-full">U</div>
       </div>
       <ul
         tabindex="0"
         class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
       >
-        <li>hi didar nawzad</li>
         <li><router-link to="/signIn">signIn</router-link></li>
-        <li><router-link to="/signup">Register</router-link></li>
+        <li><router-link to="/signup">signup</router-link></li>
         <li @click="getCurrentUser()" class="btn">current user</li>
       </ul>
     </div>
@@ -58,6 +50,7 @@ import { useRouter } from "vue-router";
 const { getCurrentUser, signOut, isAuthenticated } = useAuthenticationStore();
 
 const router = useRouter();
+
 
 const isLogged = computed(() => {
   return isAuthenticated;
