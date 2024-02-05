748bef87da195b383602ee06bde5777d97203bf7
didar <deddarnawzad@gmail.com>
2024-02-05 21:18:11 +0300

    Add UsersView component

---
+++ src/views/UsersView.vue
@@ -0,0 +1,45 @@
+<template lang="">
+  <div class="grid gap-5 grid-cols-res2">
+    <div class="card shadow-gl w-full bg-white border rounded-none">
+      <div class="card-body flex-row gap-0 p-5">
+        <div class="p-5 m-auto">
+          <img
+            src="https://i.pravatar.cc/500?img=32"
+            alt=""
+            class="rounded-full w-32 object-cover"
+          />
+        </div>
+        <div class="card-title flex flex-col w-full justify-start items-start">
+          <h2 class="">John Doe</h2>
+          <p class="text-sm text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit
+          </p>
+          <button class="btn btn-outline  rounded-none">view profile</button>
+        </div>
+      </div>
+    </div>
+    <div class="card shadow-gl w-full bg-white border rounded-none">
+      <div class="card-body flex-row gap-0 p-5">
+        <div class="p-5 m-auto">
+          <img
+            src="https://i.pravatar.cc/500?img=32"
+            alt=""
+            class="rounded-full w-32 object-cover"
+          />
+        </div>
+        <div class="card-title flex flex-col w-full justify-start items-start">
+          <h2 class="">John Doe</h2>
+          <p class="text-sm text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit
+          </p>
+          <button class="btn btn-outline  rounded-none">view profile</button>
+        </div>
+      </div>
+    </div>
+   
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
