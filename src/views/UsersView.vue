ae3f4af2611914f521ce4614cac80099f6e81df8
didar <deddarnawzad@gmail.com>
2024-02-05 21:44:08 +0300

    Refactor UsersView component and add UserCard component

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,45 +1,27 @@
 <template lang="">
   <div class="grid gap-5 grid-cols-res2">
-    <div class="card shadow-gl w-full bg-white border rounded-none">
-      <div class="card-body flex-row gap-0 p-5">
-        <div class="p-5 m-auto">
-          <img
-            src="https://i.pravatar.cc/500?img=32"
-            alt=""
-            class="rounded-full w-32 object-cover"
-          />
-        </div>
-        <div class="card-title flex flex-col w-full justify-start items-start">
-          <h2 class="">John Doe</h2>
-          <p class="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit
-          </p>
-          <button class="btn btn-outline  rounded-none">view profile</button>
-        </div>
-      </div>
-    </div>
-    <div class="card shadow-gl w-full bg-white border rounded-none">
-      <div class="card-body flex-row gap-0 p-5">
-        <div class="p-5 m-auto">
-          <img
-            src="https://i.pravatar.cc/500?img=32"
-            alt=""
-            class="rounded-full w-32 object-cover"
-          />
-        </div>
-        <div class="card-title flex flex-col w-full justify-start items-start">
-          <h2 class="">John Doe</h2>
-          <p class="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit
-          </p>
-          <button class="btn btn-outline  rounded-none">view profile</button>
-        </div>
-      </div>
-    </div>
-   
+    <UserCard v-for="user in usersState.users" :key="user.uid" :item="user" />
   </div>
 </template>
-<script>
-export default {};
+<script setup>
+import User from "@/service/firestore/User.js";
+import UserCard from "@/components/UserCard.vue";
+import { reactive, onMounted } from "vue";
+const users = new User();
+
+const usersState = reactive({
+  users: [],
+  loading: false,
+  error: null,
+});
+
+const fetchUsers = async () => {
+  const response = await users.getAllUsers();
+  usersState.users = response;
+};
+
+onMounted(() => {
+  fetchUsers();
+});
 </script>
 <style lang=""></style>
