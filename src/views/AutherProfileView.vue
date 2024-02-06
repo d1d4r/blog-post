05a111d083595ddaf6aad4ac9a0aee44b7fc3791
didar <deddarnawzad@gmail.com>
2024-02-06 22:37:36 +0300

    Refactor author profile view layout

--- src/views/AutherProfileView.vue
+++ src/views/AutherProfileView.vue
@@ -1,11 +1,11 @@
 <template lang="">
   <div>
-    <div class="grd w-full h-44">
+    <div class="w-full grd h-44">
       <img
         loading="lazy"
         :src="userState.user.photoURL"
         alt=""
-        class="object-contain h-full w-full "
+        class="object-contain w-full h-full"
       />
     </div>
     <div class="prose">
@@ -13,35 +13,38 @@
       <p>{{ userState.user.bio }}</p>
     </div>
     <div class="divider divider-neutral">posts</div>
-    <div class="grid gap-5 grid-cols-res">
+
+    <div class="grid gap-2 grid-cols-res">
       <div
-        v-for="item in 20"
-        :key="item"
+        v-for="post in postState.post"
+        :key="post.id"
         class="card w-72 bg-base-100 border-solid m-auto lg:m-0 border-[1px] border-sky-500 shadow-xl"
       >
         <div class="card-body">
           <figure>
             <img
-              src="https://i.pravatar.cc/500?img=32"
+              :src="userState.user.photoURL"
               alt="Shoes"
               class="w-8 h-8 border-2 border-white rounded-full shadow-xl"
             />
           </figure>
 
           <router-link :to="`/main/blogpost/`" class="hover:underline">
-            <h2 class="p-2 card-title">adasdasdasd</h2>
+            <h2 class="p-2 card-title">{{ post.title }}</h2>
           </router-link>
           <h2 class="pl-2 text-sm"></h2>
           <figure>
             <img
               loading="lazy"
-              src="https://i.pravatar.cc/500?img=32"
+              :src="post.imageUrl"
               alt="Shoes"
-              class="object-cover m-auto select-none h-44 w-full rounded-xl"
+              class="object-cover w-full m-auto select-none h-44 rounded-xl"
             />
           </figure>
           <div class="justify-end p-3 card-actions">
-            <div class="badge badge-outline">s</div>
+            <div class="badge badge-outline" v-for="tag in post.tags" :key="tag">
+              {{ tag }}
+            </div>
           </div>
         </div>
       </div>
@@ -50,16 +53,30 @@
 </template>
 <script setup>
 import User from "@/service/firestore/User.js";
+import Post from "@/service/firestore/Post.js";
 import { useRoute } from "vue-router";
 import { onMounted, reactive } from "vue";
 const router = useRoute();
 const user = new User();
+const post = new Post();
 
 const userState = reactive({
-  user: {},
+  user: [],
   loading: false,
   error: null,
 });
+
+const postState = reactive({
+  post: [],
+  loading: false,
+  error: null,
+});
+
+const fetchUserPosts = async () => {
+  const response = await post.getByUser(router.params.id);
+  postState.post = response;
+  console.log("🚀 ~ fetchUserPosts ~ postState.post:", postState.post);
+};
 
 const fetchUser = async () => {
   const response = await user.getUser(router.params.id);
@@ -67,6 +84,7 @@ const fetchUser = async () => {
 };
 onMounted(() => {
   fetchUser();
+  fetchUserPosts();
 });
 </script>
 <style scoped>
