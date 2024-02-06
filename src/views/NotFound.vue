b50465f9de6ab89a9702d359ebf1d90c5f3b2bfc
didar <deddarnawzad@gmail.com>
2024-02-07 00:20:59 +0300

    Refactor NotFound.vue component layout

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,12 @@
 <template lang="">
-  <div class="prose text-center flex flex-col justify-center items-center h-96">
-    <h1>404</h1>
-    <p>Page not found</p>
-    <router-link to="/" class="btn">Go back home</router-link>
+  <div class="h-full overflow-hidden translate-y-1/2 ">
+   
+      <div class="flex flex-col items-center justify-center gap-5">
+        <h1 class="text-9xl">404</h1>
+        <p class="text-5xl">Page not found</p>
+        <router-link to="/" class="w-1/2 btn">Go back home</router-link>
+      </div>
+   
   </div>
 </template>
 <script setup></script>
