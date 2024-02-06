86b1209c264e93ddb390be47401656b969c8c78c
didar <deddarnawzad@gmail.com>
2024-02-07 00:09:15 +0300

    Refactor layout component to include top navigation and use drawer state

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,17 +1,23 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template lang="">
- 
-   
-    <div class="drawer md:drawer-open ">
-      <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-      <MainContent />
-      <LeftSide />
-    </div>
- 
+  <TopNavigation />
+  <div class=" drawer md:drawer-open">
+    <input
+      id="my-drawer-4"
+      type="checkbox"
+      :checked="true"
+      class="drawer-toggle"
+    />
+    <MainContent />
+    <LeftSide />
+  </div>
 </template>
 <script setup>
 import LeftSide from "./LeftSide.vue";
 import MainContent from "./MainContent.vue";
 import TopNavigation from "./TopNavigation.vue";
+import { useDrawer } from "@/stores/openDrawer";
+
+const { isOpen } = useDrawer();
 </script>
 <style lang=""></style>
