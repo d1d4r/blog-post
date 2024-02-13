0bccd67d83dcae7014909ba6d7a1d5794fec3cba
didar <deddarnawzad@gmail.com>
2024-02-13 23:15:16 +0300

    add DrawerToggle component

---
+++ src/layouts/DrawerToggle.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <input id="my-drawer" type="checkbox" :checked="drawerState.open" class="drawer-toggle" />
+  <p @click="increment" class="test">count : {{ count }}</p>
+  <!-- <p @click="toggleDrawer">open : {{ drawerState.open }}</p> -->
+</template>
+<script setup>
+import {ref} from "vue";
+import { useDrawer } from "@/stores/openDrawer.js";
+let { drawerState, toggleDrawer } = useDrawer();
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+</script>
+<style scoped></style>
