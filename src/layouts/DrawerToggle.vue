3130a5c53a0d8096c85d14ad4325b261f29c44fc
didar <deddarnawzad@gmail.com>
2024-02-13 23:36:20 +0300

    done done doneeeeeeeeeeeee, hahahahha

--- src/layouts/DrawerToggle.vue
+++ src/layouts/DrawerToggle.vue
@@ -1,17 +1,19 @@
 <template lang="">
-  <input id="my-drawer" type="checkbox" :checked="drawerState.open" class="drawer-toggle" />
-  <p @click="increment" class="test">count : {{ count }}</p>
+  <input
+    id="my-drawer"
+    type="checkbox"
+    :checked="drawerState.open"
+    class="drawer-toggle"
+    v-model="drawerState.open"
+    @change="toggleDrawer"
+  />
+  <!-- <p @click="increment" class="test">count : {{ count }}</p> -->
   <!-- <p @click="toggleDrawer">open : {{ drawerState.open }}</p> -->
 </template>
 <script setup>
-import {ref} from "vue";
 import { useDrawer } from "@/stores/openDrawer.js";
-let { drawerState, toggleDrawer } = useDrawer();
-const count = ref(0);
+const { drawerState, toggleDrawer } = useDrawer();
 
-const increment = () => {
-  count.value++;
-};
 
 </script>
 <style scoped></style>
