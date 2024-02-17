a26fd5a59d285884fd521f20591f8eccaf485b53
didar <deddarnawzad@gmail.com>
2024-02-17 23:07:09 +0300

    add pagination component

--- src/views/MyFeedView.vue
+++ src/views/MyFeedView.vue
@@ -1,9 +1,39 @@
 <template lang="">
-  <div
-    v-if="!blogPostState.loading"
-    class="grid gap-2 justify-items-center grid-cols-res"
-  >
-    <CardPost v-for="item in blogPostState.posts" :key="item.id" :item="item" />
+  <div v-if="!blogPostState.loading" class="flex flex-col ">
+    <div class="grid gap-2 justify-items-center grid-cols-res">
+      <CardPost
+        v-for="item in blogPostState.posts"
+        :key="item.id"
+        :item="item"
+      />
+    </div>
+    <div class="p-5 m-auto join">
+      <input
+        class="join-item btn btn-square"
+        type="radio"
+        name="options"
+        aria-label="1"
+        checked
+      />
+      <input
+        class="join-item btn btn-square"
+        type="radio"
+        name="options"
+        aria-label="2"
+      />
+      <input
+        class="join-item btn btn-square"
+        type="radio"
+        name="options"
+        aria-label="3"
+      />
+      <input
+        class="join-item btn btn-square"
+        type="radio"
+        name="options"
+        aria-label="4"
+      />
+    </div>
   </div>
 
   <div v-else class="grid gap-2 grid-cols-res">
@@ -14,7 +44,7 @@
 import CardPost from "@/components/CardPost.vue";
 import SkeletonCardPost from "@/components/SkeletonCardPost.vue";
 //import { app } from "@/firebase/index.js";
-import {  onMounted } from "vue";
+import { onMounted } from "vue";
 import { useBlogPostStore } from "@/stores/useBlogPostStore";
 
 const { blogPostState, fetchPosts } = useBlogPostStore();
