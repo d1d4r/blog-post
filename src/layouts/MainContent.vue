356e924a9bf3888ab9a778582aa497a47b19a47f
didar <deddarnawzad@gmail.com>
2024-02-18 23:16:41 +0300

    remove unnnesesory useRoye hook and key

--- src/layouts/MainContent.vue
+++ src/layouts/MainContent.vue
@@ -1,19 +1,23 @@
 <template lang="">
-  <div class="p-5 drawer-content">
-    <main class="h-full">
+  <div class="p-5 bg-white drawer-content">
+    <main class="h-full ">
       <router-view></router-view>
-      <router-view name="MainContent"></router-view>
-      <router-view name="ProfileView"></router-view>
-      <router-view name="HowTo"></router-view>
-      <router-view name="BookMarks"></router-view>
-      <router-view name="BlogPost"></router-view>
-      <router-view name="UserProfile"></router-view>
-      <router-view name="Users"></router-view>
-      <router-view name="CreatePost"></router-view>
+      <router-view  name="MainContent"></router-view>
+      <router-view  name="ProfileView"></router-view>
+      <router-view  name="HowTo"></router-view>
+      <router-view  name="BookMarks"></router-view>
+      <router-view  name="BlogPost"></router-view>
+      <router-view  name="UserProfile"></router-view>
+      <router-view  name="Users"></router-view>
+      <router-view  name="CreatePost"></router-view>
     </main>
   </div>
 </template>
-<script>
-export default {};
+<script setup>
+
+
+
+
+
 </script>
 <style lang=""></style>
