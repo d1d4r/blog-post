48252072ddcc6038fc1dddcf5818f0d7aa9cf6a3
didar <deddarnawzad@gmail.com>
2024-02-18 23:15:02 +0300

    add loading state show skeleton and change style post

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,13 +1,26 @@
 <template lang="">
   <div class="flex flex-col items-center justify-center">
-    <div class="prose">
+    <div class="flex flex-col w-1/2 gap-5" v-if="state.loading">
+      <BookMarkButton :data="state.postItem" :disabled="state.loading" />
+      <div class="w-full h-4 skeleton"></div>
+      <div class="w-full h-96 skeleton"></div>
+      <div class="h-4 skeleton w-28"></div>
+      <div class="w-full h-4 skeleton"></div>
+      <div class="w-full h-4 skeleton"></div>
+    </div>
+    <div class="w-1/2" v-else>
       <BookMarkButton :data="state.postItem" />
-      <h1 class="text-center">{{ state.postItem.title }}</h1>
+
+      <h1 class="p-5 text-4xl text-center">{{ state.postItem.title }}</h1>
       <figure>
-        <img :src="state.postItem.imageUrl" alt="" />
+        <img
+          class="object-cover w-full h-96"
+          :src="state.postItem.imageUrl"
+          alt=""
+        />
         <!-- <figcaption>Photo by John Doe</figcaption> -->
       </figure>
-      <p class="leading-loose tracking-wide hyphens-auto">
+      <p class="text-xl leading-loose tracking-wide hyphens-auto">
         {{ state.postItem.content }}
       </p>
     </div>
@@ -38,8 +51,10 @@ const state = reactive({
 });
 
 onMounted(async () => {
+  state.loading = true;
   const postItem = await post.get(route.params.id);
   state.postItem = postItem;
+  state.loading = false;
 });
 </script>
 <style lang=""></style>
