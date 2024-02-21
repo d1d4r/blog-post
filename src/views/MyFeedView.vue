14b155992668282e124fbe684638b773e6d6e72d
didar <deddarnawzad@gmail.com>
2024-02-22 00:45:45 +0300

    add pagination component

--- src/views/MyFeedView.vue
+++ src/views/MyFeedView.vue
@@ -1,78 +1,112 @@
 <template lang="">
-  <div v-if="!blogPostState.loading" class="flex flex-col">
-    <div class="grid gap-2 justify-items-center grid-cols-res">
-      <CardPost
-        v-for="item in blogPostState.posts"
-        :key="item.id"
-        :item="item"
-      />
+  <div class="h-full ">
+    <div v-if="!state.loading" class="flex flex-col">
+      <div class="grid gap-2 justify-items-center grid-cols-res">
+        <CardPost v-for="item in state.posts" :key="item.id" :item="item" />
+      </div>
+      <div class="p-5 m-auto">
+        <OffsetPagination />
+      </div>
     </div>
-    <div class="p-5 m-auto">
-      <OffsetPagination/>
+    <div v-else class="grid gap-2 grid-cols-res h-96">
+      <SkeletonCardPost v-for="item in 4" :key="item.id" />
     </div>
   </div>
-
-  <div v-else class="grid gap-2 grid-cols-res">
-    <SkeletonCardPost v-for="item in 200" :key="item.id" />
-  </div>
+  <UseOffsetPagination>
+    <div class="flex items-center justify-center gap-2">
+      <button
+        :disabled="isFirstPage"
+        @click="prev"
+        class="join-item btn btn-outline"
+      >
+        prev
+      </button>
+      <button
+        type="button"
+        class="border border-black disabled:border-base-100 btn btn-md bg-base-100 disabled:bg-black disabled:text-base-100 disabled:cursor-not-allowed"
+        v-for="item in pageCount"
+        :key="item"
+        :disabled="currentPage === item"
+        @click="currentPage = item"
+      >
+        {{ item }}
+      </button>
+      <button
+        :disabled="isLastPage"
+        @click="next"
+        class="join-item btn btn-outline"
+      >
+        next
+      </button>
+    </div>
+  </UseOffsetPagination>
 </template>
 
 <script setup>
 import CardPost from "@/components/CardPost.vue";
 import SkeletonCardPost from "@/components/SkeletonCardPost.vue";
-//import { app } from "@/firebase/index.js";
-import { onMounted, reactive } from "vue";
-//import { useBlogPostStore } from "@/stores/useBlogPostStore";
-import OffsetPagination from "@/components/OffsetPagination.vue";
-import Post from "@/service/firestore/Post.js";
-//import { useOffsetPagination } from "@vueuse/core";
-//const { blogPostState, fetchPosts } = useBlogPostStore();
+import { useOffsetPagination } from "@vueuse/core";
+import { reactive, computed } from "vue";
+import { UseOffsetPagination } from "@vueuse/components";
+import Post from "@/service/firestore/Post";
+import { app } from "@/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const post = new Post();
+const db = getFirestore(app);
 
-const blogPostState = reactive({
+const state = reactive({
   posts: [],
-  total: 0,
+  total: null,
   page: 1,
-  pageSize: 1,
-  loading: true,
+  pageSize: 2,
+  lastDoc: null,
+  loading: false,
 });
 
-
-
-const fetchPosts = async () => {
-  blogPostState.loading = true;
+const fetchPost = async ({ currentPage, currentPageSize }) => {
   try {
-    const  posts = await post.getAll();
- 
-    blogPostState.posts = posts;
-    
+    state.loading = true;
+    const { posts, total } = await post.paginatePosts(
+      db,
+      "Posts",
+      currentPage,
+      currentPageSize
+    );
+
+    state.total = total;
+    state.posts = posts;
+    //state.lastDoc = last;
+
+    return posts;
   } catch (error) {
-    console.error(error);
+    console.log("🚀 ~ fetchPost ~ error:", error);
   } finally {
-    blogPostState.loading = false;
+    state.loading = false;
   }
 };
-onMounted(async () => {
-  fetchPosts(blogPostState.page, blogPostState.pageSize);
+
+fetchPost({ currentPage: state.page, currentPageSize: state.pageSize });
+
+const t = computed(() => {
+  return state.total;
 });
-// const {
-//   currentPage,
-//   currentPageSize,
-//   pageCount,
-//   isFirstPage,
-//   isLastPage,
-//   prev,
-//   next,
-// } = useOffsetPagination({
-//   total: blogPostState.total,
-//   page: 1,
-//   pageSize: blogPostState.pageSize,
-//   onPageChange: fetchPosts,
-//   onPageSizeChange: fetchPosts,
-// });
 
-
+const {
+  currentPage,
+  currentPageSize,
+  pageCount,
+  isFirstPage,
+  isLastPage,
+  prev,
+  next,
+} = useOffsetPagination({
+  total: t,
+  page: state.page,
+  pageSize: state.pageSize,
+  onPageChange: fetchPost,
+  onPageSizeChange: fetchPost,
+});
 </script>
 <style lang=""></style>
 @/service/firestore/Post.js
