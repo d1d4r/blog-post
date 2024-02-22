0a0116a633c75584846abaeed40442fc023c5270
didar <deddarnawzad@gmail.com>
2024-02-23 00:22:17 +0300

    remove form inputs, add markdown editor

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="text-sm breadcrumbs">
-    <ul>
+    <!-- <ul>
       <li><router-link to="/profile">Profile</router-link></li>
       <li>Create Post</li>
-    </ul>
+    </ul> -->
   </div>
   <div class="flex flex-col items-center justify-center">
-    <h1 class="text-2xl font-bold">Create Post</h1>
-    <form @submit="handleSubmit">
+    <h1 class="mb-5 text-2xl font-bold">Create Post</h1>
+    <md-editor
+      language="en-US"
+      v-model="markdown.content"
+      
+      @onUploadImg="onUploadImg"
+      @onSave="onSave"
+      class="min-h-screen"
+    />
+    <!-- <form @submit="handleSubmit">
       <label class="w-full max-w-xs form-control" for="title">
         <div class="label">
           <span class="label-text">Title</span>
@@ -77,17 +85,28 @@
         <button type="submit" class="mt-3 btn btn-primary">Submit</button>
         <button type="reset" class="mt-3 btn btn-secondary">Cancel</button>
       </div>
-    </form>
+    </form> -->
   </div>
 </template>
 
 <script setup>
 import { reactive } from "vue";
-//import { app } from "../firebase";
-import Post from "@/service/firestore/post";
+import Post from "@/service/firestore/Post";
 import UploadPostImage from "@/service/storage/uploadPostImage";
 import { useRouter } from "vue-router";
 import { useAuthenticationStore } from "@/stores/useAuthenticationStore.js";
+import { MdEditor, config } from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+import "md-editor-v3/lib/preview.css";
+import EN_US from "@/english.js";
+
+config({
+  editorConfig: {
+    languageUserDefined: {
+      "en-US": EN_US,
+    },
+  },
+});
 
 const { user } = useAuthenticationStore();
 
@@ -95,6 +114,33 @@ const router = useRouter();
 const post = new Post();
 const uploadPostImage = new UploadPostImage();
 
+const markdown = reactive({
+  content: "",
+});
+
+const onChange = () => {
+  console.log(markdown.content);
+};
+
+const onUploadImg = async (file) => {
+  const [type] = file;
+
+  const { progress, downloadURL } = await uploadPostImage.upload(type);
+  // file: file[0],
+  //   path: `post/${user.uid}/${name}`,
+  fileData.progress = progress;
+  formData.imageUrl = downloadURL;
+  markdown.content += `![${file[0].name}](${downloadURL})`;
+};
+
+const onSave = async () => {
+  console.log(markdown.content);
+  //e.preventDefault();
+  const postId = await post.create(markdown);
+  router.push(`/main/blogpost/${postId}`);
+};
+
+///////////////////////////////////////
 const categories = [
   "web development",
   "frontend development",
@@ -118,6 +164,7 @@ const fileData = reactive({
 
 const handleFileChange = async (e) => {
   fileData.file = e.target.files[0];
+  console.log(fileData.file);
   const { progress, downloadURL } = await uploadPostImage.upload(fileData.file);
   fileData.progress = progress;
   formData.imageUrl = downloadURL;
