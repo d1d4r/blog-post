90c2404fa3d692b899f03f19225a87e801a73b9f
didar <deddarnawzad@gmail.com>
2024-02-24 00:05:17 +0300

    comment out unused methode

--- src/service/firestore/post.js
+++ src/service/firestore/post.js
@@ -20,141 +20,6 @@ class Post {
     this.db = getFirestore(app);
   }
 
-  async getAll() {
-    try {
-      const posts = [];
-
-      const postsRef = collection(this.db, "Posts");
-      const first = query(postsRef, limit(3));
-      const querySnapshot = await getDocs(first);
-
-      querySnapshot.forEach((doc) => {
-        posts.push({ ...doc.data(), id: doc.id });
-      });
-
-      return posts;
-    } catch (error) {
-      console.log("🚀 ~ Post ~ getAll ~ error", error);
-      return error;
-    }
-  }
-  async finalPagination(
-    currentPage,
-    currentPageSize,
-    startDocReference = null
-  ) {
-    try {
-      const postRef = collection(this.db, "Posts");
-      const total = (await getCountFromServer(postRef)).data().count;
-      //const last = total / currentPageSize;
-      let q;
-      if (startDocReference) {
-        q = query(
-          postRef,
-          orderBy("title"),
-          startAfter(startDocReference),
-          limit(currentPageSize)
-        );
-      } else {
-        q = query(postRef, orderBy("title"), limit(currentPageSize));
-      }
-      const snapshot = await getDocs(q);
-      const last = snapshot.docs[snapshot.docs.length - 1].id;
-      if (snapshot.empty) {
-        return null; // No documents found
-      }
-      const posts = [];
-      snapshot.forEach((doc) => {
-        posts.push({ ...doc.data(), id: doc.id });
-      });
-      return { posts, total, last };
-    } catch (error) {
-      console.log("🚀 ~ Post ~ finalPagination ~ error", error);
-      return error;
-    }
-  }
-
-  async getPaginationOffcet(currentPage, currentPageSize) {
-    try {
-      const posts = [];
-      const postsRef = collection(this.db, "Posts");
-      const total = (await getCountFromServer(postsRef)).data().count;
-      const first = query(postsRef, limit(currentPageSize));
-      const querySnapshot = await getDocs(first);
-      const lastDocument = querySnapshot.docs[querySnapshot.docs.length - 1].id;
-
-      querySnapshot.forEach((doc) => {
-        posts.push({ ...doc.data(), id: doc.id });
-      });
-      return { posts, total, lastDocument };
-    } catch (error) {
-      console.log("🚀 ~ Post ~ getAll ~ error", error);
-      return error;
-    }
-  }
-  async loadDocuments(lastDocument, currentPageSize) {
-    console.log(
-      "🚀 ~ Post ~ loadDocuments ~ lastDocument, currentPageSiz:",
-      lastDocument,
-      currentPageSize
-    );
-    const after = lastDocument;
-    try {
-      const posts = [];
-      const quer = query(
-        collection(this.db, "Posts"),
-        orderBy("title"),
-        startAfter(after),
-        limit(1)
-      );
-
-      const snapshot = await getDocs(quer);
-      const lastDocument = snapshot.docs[snapshot.docs.length - 1].id;
-
-      if (snapshot.empty) {
-        console.log("No documents found");
-        return;
-      }
-
-      snapshot.forEach((doc) => {
-        posts.push({ ...doc.data(), id: doc.id });
-      });
-      console.log("🚀 ~ Post ~ loadDocuments ~ posts:", posts);
-      return { posts, lastDocument };
-    } catch (error) {
-      console.error("Error fetching documents:", error);
-    }
-  }
-  // async loadMore() {
-  //   try {
-  //     const quer = query(
-  //       collection(this.db, "Posts"),
-  //       orderBy("title"),
-  //       startAfter(lastDocument),
-  //       limit(pageSize)
-  //     );
-
-  //     const snapshot = await getDocs(quer);
-
-  //     if (snapshot.empty) {
-  //       console.log("No more documents to load");
-  //       return;
-  //     }
-
-  //     lastDocument = snapshot.docs[snapshot.docs.length - 1];
-
-  //     documents.value = [
-  //       ...documents.value,
-  //       ...snapshot.docs.map((doc) => ({
-  //         id: doc.id,
-  //         data: doc.data(),
-  //       })),
-  //     ];
-  //   } catch (error) {
-  //     console.error("Error loading more documents:", error);
-  //   }
-  // }
-
   async get(id) {
     const docRef = doc(this.db, "Posts", id);
 
@@ -239,7 +104,6 @@ class Post {
       // Extract and format data, calculate nextPageToken
       const posts = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
-    
       const postarr = [];
       snapshot.docs.forEach((doc) => {
         const inputString = doc.data().content;
@@ -256,8 +120,8 @@ class Post {
           title: hashText,
           imageUrl: exclamationText,
           id: doc.id,
+          userId : doc.data().userId
         });
-
       });
       //const total = await this.getTotalCount(db, collectionPath);
       const total = (await getCountFromServer(postsRef)).data().count;
@@ -287,12 +151,146 @@ class Post {
 
     return snapshot.docs[index];
   }
-
-  // Helper function to get total document count
-  async getTotalCount(db, collectionPath) {
-    const snapshot = await getDocs(collection(db, collectionPath));
-    return snapshot.size;
-  }
 }
 
 export default Post;
+
+// Helper function to get total document count
+// async getTotalCount(db, collectionPath) {
+//   const snapshot = await getDocs(collection(db, collectionPath));
+//   return snapshot.size;
+// }
+// async getAll() {
+//   try {
+//     const posts = [];
+
+//     const postsRef = collection(this.db, "Posts");
+//     const first = query(postsRef, limit(3));
+//     const querySnapshot = await getDocs(first);
+
+//     querySnapshot.forEach((doc) => {
+//       posts.push({ ...doc.data(), id: doc.id });
+//     });
+
+//     return posts;
+//   } catch (error) {
+//     console.log("🚀 ~ Post ~ getAll ~ error", error);
+//     return error;
+//   }
+// }
+// async finalPagination(
+//   currentPage,
+//   currentPageSize,
+//   startDocReference = null
+// ) {
+//   try {
+//     const postRef = collection(this.db, "Posts");
+//     const total = (await getCountFromServer(postRef)).data().count;
+//     //const last = total / currentPageSize;
+//     let q;
+//     if (startDocReference) {
+//       q = query(
+//         postRef,
+//         orderBy("title"),
+//         startAfter(startDocReference),
+//         limit(currentPageSize)
+//       );
+//     } else {
+//       q = query(postRef, orderBy("title"), limit(currentPageSize));
+//     }
+//     const snapshot = await getDocs(q);
+//     const last = snapshot.docs[snapshot.docs.length - 1].id;
+//     if (snapshot.empty) {
+//       return null; // No documents found
+//     }
+//     const posts = [];
+//     snapshot.forEach((doc) => {
+//       posts.push({ ...doc.data(), id: doc.id });
+//     });
+//     return { posts, total, last };
+//   } catch (error) {
+//     console.log("🚀 ~ Post ~ finalPagination ~ error", error);
+//     return error;
+//   }
+// }
+
+// async getPaginationOffcet(currentPage, currentPageSize) {
+//   try {
+//     const posts = [];
+//     const postsRef = collection(this.db, "Posts");
+//     const total = (await getCountFromServer(postsRef)).data().count;
+//     const first = query(postsRef, limit(currentPageSize));
+//     const querySnapshot = await getDocs(first);
+//     const lastDocument = querySnapshot.docs[querySnapshot.docs.length - 1].id;
+
+//     querySnapshot.forEach((doc) => {
+//       posts.push({ ...doc.data(), id: doc.id });
+//     });
+//     return { posts, total, lastDocument };
+//   } catch (error) {
+//     console.log("🚀 ~ Post ~ getAll ~ error", error);
+//     return error;
+//   }
+// }
+// async loadDocuments(lastDocument, currentPageSize) {
+//   console.log(
+//     "🚀 ~ Post ~ loadDocuments ~ lastDocument, currentPageSiz:",
+//     lastDocument,
+//     currentPageSize
+//   );
+//   const after = lastDocument;
+//   try {
+//     const posts = [];
+//     const quer = query(
+//       collection(this.db, "Posts"),
+//       orderBy("title"),
+//       startAfter(after),
+//       limit(1)
+//     );
+
+//     const snapshot = await getDocs(quer);
+//     const lastDocument = snapshot.docs[snapshot.docs.length - 1].id;
+
+//     if (snapshot.empty) {
+//       console.log("No documents found");
+//       return;
+//     }
+
+//     snapshot.forEach((doc) => {
+//       posts.push({ ...doc.data(), id: doc.id });
+//     });
+//     console.log("🚀 ~ Post ~ loadDocuments ~ posts:", posts);
+//     return { posts, lastDocument };
+//   } catch (error) {
+//     console.error("Error fetching documents:", error);
+//   }
+// }
+// async loadMore() {
+//   try {
+//     const quer = query(
+//       collection(this.db, "Posts"),
+//       orderBy("title"),
+//       startAfter(lastDocument),
+//       limit(pageSize)
+//     );
+
+//     const snapshot = await getDocs(quer);
+
+//     if (snapshot.empty) {
+//       console.log("No more documents to load");
+//       return;
+//     }
+
+//     lastDocument = snapshot.docs[snapshot.docs.length - 1];
+
+//     documents.value = [
+//       ...documents.value,
+//       ...snapshot.docs.map((doc) => ({
+//         id: doc.id,
+//         data: doc.data(),
+//       })),
+//     ];
+//   } catch (error) {
+//     console.error("Error loading more documents:", error);
+//   }
+// }
