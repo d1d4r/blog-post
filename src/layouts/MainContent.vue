d8c20e1cb0013577c078d6378b6990b277f2c49a
didar <deddarnawzad@gmail.com>
2024-02-27 23:08:19 +0300

    remove all named route replace by one defualt route-view

--- src/layouts/MainContent.vue
+++ src/layouts/MainContent.vue
@@ -1,23 +1,19 @@
 <template lang="">
-  <div class="p-5 bg-white drawer-content">
-    <main class="h-full ">
+  <div class="bg-white drawer-content ">
+    <main class="h-full">
       <router-view></router-view>
-      <router-view  name="MainContent"></router-view>
-      <router-view  name="ProfileView"></router-view>
-      <router-view  name="HowTo"></router-view>
-      <router-view  name="BookMarks"></router-view>
-      <router-view  name="BlogPost"></router-view>
-      <router-view  name="UserProfile"></router-view>
-      <router-view  name="Users"></router-view>
-      <router-view  name="CreatePost"></router-view>
+     
     </main>
   </div>
 </template>
-<script setup>
-
-
-
-
-
-</script>
+<script setup></script>
 <style lang=""></style>
+<!-- 
+<router-view name="MainContent"></router-view>
+<router-view name="ProfileView"></router-view>
+<router-view name="HowTo"></router-view>
+<router-view name="BookMarks"></router-view>
+<router-view name="BlogPost"></router-view>
+<router-view name="UserProfile"></router-view>
+<router-view name="Users"></router-view>
+<router-view name="CreatePost"></router-view>  -->
