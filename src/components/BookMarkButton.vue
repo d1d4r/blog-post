bdb6052e800d1bd956c28e95a40bcf2ee27a2aa7
didar <deddarnawzad@gmail.com>
2024-02-27 23:07:02 +0300

    add heroicon, and pass id props, add fetchSavedData method

--- src/components/BookMarkButton.vue
+++ src/components/BookMarkButton.vue
@@ -1,40 +1,45 @@
 <template lang="">
-  <button class="btn btn-square btn-outline" @click="addBookMark">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-      />
-    </svg>
+  <button class="btn btn-square btn-outline" v-if="isSaved">
+    <BookmarkSlashIcon class="size-10" />
+  </button>
+  <button class="btn btn-square btn-outline" @click="addBookMark" v-else>
+    <BookmarkIcon class="size-10" />
   </button>
 </template>
 <script setup>
 import BookMark from "@/service/firestore/BookMark";
 import { useAuthenticationStore } from "@/stores/useAuthenticationStore";
+import { BookmarkIcon, BookmarkSlashIcon } from "@heroicons/vue/16/solid";
+import { ref } from "vue";
+
 const props = defineProps({
   data: {
     type: Object,
     required: true,
+  },
+  id: {
+    type: String,
   },
 });
 
 const { user } = useAuthenticationStore();
 const bookmark = new BookMark();
 
-const addBookMark = () => {
+const isSaved = ref(false);
+const addBookMark = async () => {
   try {
-    bookmark.addBookMark(user.uid, props.data);
+    await bookmark.addBookMark(user.uid, {
+      data: props.data,
+      id: props.id,
+    });
   } catch (error) {
     console.log("🚀 ~ addBookMark ~ error:", error);
   }
 };
+const fetchSavedData = async () => {
+  const save = await bookmark.fetchBookMarkById(props.id, user.uid);
+  isSaved.value = save;
+};
+fetchSavedData();
 </script>
 <style lang=""></style>
