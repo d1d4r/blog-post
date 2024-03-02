68eb504a8208d09296655d9debfe7bac86bbc718
didar <deddarnawzad@gmail.com>
2024-03-02 23:14:35 +0300

    refactor myfeedview component and display pagination conditionaly

--- src/views/MyFeedView.vue
+++ src/views/MyFeedView.vue
@@ -1,51 +1,53 @@
 <template lang="">
   <div class="min-h-screen mt-24">
-    <Transition>
-      <div v-if="!state.loading" class="flex flex-col">
-        <div class="grid gap-2 justify-items-center grid-cols-res">
-          <CardPost v-for="item in state.posts" :key="item.id" :item="item" />
-        </div>
+    <div v-if="!state.loading" class="flex flex-col">
+      <div class="grid gap-2 justify-items-center grid-cols-res">
+        <CardPost v-for="item in state.posts" :key="item.id" :item="item" />
       </div>
-      <!-- <div v-else class="grid gap-2 grid-cols-res h-96">
-        <SkeletonCardPost v-for="item in 4" :key="item.id" />
-      </div> -->
-    </Transition>
-  </div>
-  <UseOffsetPagination>
-    <div class="flex items-center justify-center gap-2">
-      <button
-        :disabled="isFirstPage"
-        @click="prevTop"
-        class="join-item btn btn-outline"
-      >
-        prev
-      </button>
-      <button
-        type="button"
-        class="border border-black disabled:border-base-100 btn btn-md bg-base-100 disabled:bg-black disabled:text-base-100 disabled:cursor-not-allowed"
-        v-for="item in pageCount"
-        :key="item"
-        :disabled="currentPage === item"
-        @click="currentPage = item"
-      >
-        {{ item }}
-      </button>
-      <button
-        :disabled="isLastPage"
-        @click="nextTop"
-        class="join-item btn btn-outline"
-      >
-        next
-      </button>
     </div>
-  </UseOffsetPagination>
+    <div v-else class="grid gap-2 grid-cols-res h-96">
+      <SkeletonCardPost v-for="item in 4" :key="item.id" />
+    </div>
+
+    <UseOffsetPagination>
+      <div
+        class="flex items-center justify-center gap-2 mt-10"
+        :class="{ hidden: state.loading }"
+      >
+        <button
+          :disabled="isFirstPage"
+          @click="prevTop"
+          class="join-item btn btn-outline"
+        >
+          prev
+        </button>
+        <button
+          type="button"
+          class="border border-black disabled:border-base-100 btn btn-md bg-base-100 disabled:bg-black disabled:text-base-100 disabled:cursor-not-allowed"
+          v-for="item in pageCount"
+          :key="item"
+          :disabled="currentPage === item"
+          @click="currentPage = item"
+        >
+          {{ item }}
+        </button>
+        <button
+          :disabled="isLastPage"
+          @click="nextTop"
+          class="join-item btn btn-outline"
+        >
+          next
+        </button>
+      </div>
+    </UseOffsetPagination>
+  </div>
 </template>
 
 <script setup>
 import CardPost from "@/components/CardPost.vue";
 import SkeletonCardPost from "@/components/SkeletonCardPost.vue";
 import { useOffsetPagination } from "@vueuse/core";
-import { reactive, computed } from "vue";
+import { reactive, computed, ref } from "vue";
 import { UseOffsetPagination } from "@vueuse/components";
 import Post from "@/service/firestore/Post";
 import { app } from "@/firebase";
@@ -55,7 +57,7 @@ import User from "@/service/firestore/User";
 
 const { getUserInfo } = useAuthenticationStore();
 const post = new Post();
-const user = new User()
+const user = new User();
 
 const db = getFirestore(app);
 
@@ -63,7 +65,7 @@ const state = reactive({
   posts: [],
   total: null,
   page: 1,
-  pageSize: 3,
+  pageSize: 5,
   lastDoc: null,
   loading: false,
 });
@@ -95,7 +97,9 @@ const fetchPost = async ({ currentPage, currentPageSize }) => {
     const posts = [];
     for (let post = 0; post < postarr.length; post++) {
       //console.log(postarr[post].userId)
-      const { displayName, photoURL } = await user.getUser(postarr[post].userId);
+      const { displayName, photoURL } = await user.getUser(
+        postarr[post].userId
+      );
       posts.push({
         displayName,
         photoURL,
