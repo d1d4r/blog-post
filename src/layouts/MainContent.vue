df4b3ff1b85848d8558e60ae4b09817c65bb32b8
didar <deddarnawzad@gmail.com>
2024-03-05 00:02:25 +0300

    remove comments and grd class style

--- src/layouts/MainContent.vue
+++ src/layouts/MainContent.vue
@@ -1,19 +1,12 @@
 <template lang="">
   <div class="bg-white drawer-content ">
-    <main class="h-full">
+    <main class="">
       <router-view></router-view>
-     
     </main>
   </div>
 </template>
 <script setup></script>
-<style lang=""></style>
-<!-- 
-<router-view name="MainContent"></router-view>
-<router-view name="ProfileView"></router-view>
-<router-view name="HowTo"></router-view>
-<router-view name="BookMarks"></router-view>
-<router-view name="BlogPost"></router-view>
-<router-view name="UserProfile"></router-view>
-<router-view name="Users"></router-view>
-<router-view name="CreatePost"></router-view>  -->
+<style scoped>
+
+</style>
+
