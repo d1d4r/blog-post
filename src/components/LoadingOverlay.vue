c542a3f405a299ae29026a20c359d5e54be5dc97
didar <deddarnawzad@gmail.com>
2024-03-05 00:03:49 +0300

    delete LoadingOverlay component

--- src/components/LoadingOverlay.vue
+++
@@ -1,8 +0,0 @@
-<template lang="">
-  <div class="absolute inset-0 rounded-md backdrop-blur-lg "></div>
-  <div class="absolute inset-0 flex items-center justify-center">
-    <h2 class="text-3xl font-bold text-whitez">Loading...</h2>
-  </div>
-</template>
-<script setup></script>
-<style lang=""></style>
