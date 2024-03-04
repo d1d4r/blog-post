30ac24de806ff44a2592be1b0ecc38d37a1f6c68
didar <deddarnawzad@gmail.com>
2024-03-05 00:02:57 +0300

    delete hawto view

--- src/views/HowTo.vue
+++
@@ -1,9 +0,0 @@
-<template lang="">
-  <div class="flex flex-col items-center justify-center w-full gap-5 m-auto">
-    <TestPag />
-  </div>
-</template>
-<script setup>
-import TestPag from "@/components/testPag.vue";
-</script>
-<style lang=""></style>
