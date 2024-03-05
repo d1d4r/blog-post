a8e37fcdde0d899f73b88218965f626f4ae037d9
didar <deddarnawzad@gmail.com>
2024-03-05 04:12:05 +0300

    add vercel/speed-insights package

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-
+  <SpeedInsights />
   <TopNavigation />
-  <Layout >
-    
-  </Layout>
-
+  <Layout> </Layout>
 </template>
 <script setup>
 import TopNavigation from "./layouts/TopNavigation.vue";
 import { useAuthenticationStore } from "@/stores/useAuthenticationStore.js";
 import { onBeforeMount } from "vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 import Layout from "./layouts/Layout.vue";
 
 const { tryLoadingUser } = useAuthenticationStore();
@@ -17,8 +15,6 @@ const { tryLoadingUser } = useAuthenticationStore();
 onBeforeMount(() => {
   tryLoadingUser();
 });
-
-
 </script>
 
 <style scoped></style>
